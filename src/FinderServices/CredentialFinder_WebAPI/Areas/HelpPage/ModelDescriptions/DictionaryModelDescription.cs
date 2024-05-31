8aa8e170192b60025209323570c9d6a344c2a994
Michael Parsons <58825866+mparsons-ce@users.noreply.github.com>
2024-05-31 11:14:31 -0500

    Delete src/FinderServices/CredentialFinder_WebAPI directory

--- src/FinderServices/CredentialFinder_WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CredentialFinderWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- src/FinderServices/CredentialFinder_WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CredentialFinderWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
